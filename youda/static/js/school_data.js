91c8b1473ae0f2d58d39eda18745cbbc9da0ca07
yangz <490412528@qq.com>
2015-07-15 22:51:43 +0800

    create school_data.js , which will be used to complete de register page

---
+++ youda/static/js/school_data.js
@@ -0,0 +1,66 @@
+var school = [
+{
+    school_id : '1' ,
+    school_name : '华南农业大学'
+},
+{
+    school_id : '2' ,
+    school_name : '华中科技大学'
+},
+{
+    school_id : '3' ,
+    school_name : '南京邮电大学'
+}
+];
+
+
+var collage = [
+    {
+        school_id :  '1',
+        collages : [
+            {
+                collage_id: '11',
+                collage_name: '电子学院'
+             },
+            {
+                collage_id: '12',
+                collage_name:' 电子学院'
+            },
+            {
+                collage_id: '13',
+                collage_name: '电子学院'
+              }]
+    }, {
+        school_id :  '2',
+        collages : [
+            {
+                collage_id: '21',
+                collage_name: '电子学院'
+             },
+            {
+                collage_id: '32',
+                collage_name:' 电子学院'
+            },
+            {
+                collage_id: '33',
+                collage_name: '电子学院'
+              }]
+    },
+    {
+        school_id :  '3',
+        collages : [
+            {
+                collage_id: '31',
+                collage_name: '电子学院'
+             },
+            {
+                collage_id: '32',
+                collage_name:' 电子学院'
+            },
+            {
+                collage_id: '33',
+                collage_name: '电子学院'
+              }]
+    }
+
+];
